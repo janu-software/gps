757fda0c320d3d0822b0a95ead316ce961705cd0
Stanislav Janů <info@lweb.cz>
2021-03-08 13:46:54 +0100

    phpdoc remove

--- src/GpsPointException.php
+++ src/GpsPointException.php
@@ -1,15 +1,10 @@
 <?php
-
-/**
- * This file is part of the GPS
- * Copyright (c) 2019 Stanislav Janů (https://www.lweb.cz)
- */
-
 declare(strict_types=1);
 
 namespace JCode\GPS;
 
 use Exception;
+
 class GpsPointException extends Exception
 {
 }
